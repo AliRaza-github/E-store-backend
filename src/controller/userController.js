ae961abfe5d87ee1f2f91deb68fec822825e40da
AliRaza-git <ar0005535@gmail.com>
2023-11-05 00:53:52 +0500

    commit

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -4,14 +4,12 @@ const User = require("../model/userModel");
 const salt = process.env.SALT;
 
 const register = async (req, res) => {
-    const { first_name, last_name, email, password } = req.body
-
+    const { first_name, last_name, email, password } = req.body;
     try {
         const userData = await User.findOne({ email: email }).select({ email: 1 })
         if (userData) {
             return res.status(400).json({ error: "Email already exist", data: null, message: "Email already exist" });
         }
-
         const hashPassword = await bcrypt.hash(password, salt);
         const user = new User({
             first_name: first_name,
@@ -19,28 +17,24 @@ const register = async (req, res) => {
             email: email,
             password: hashPassword,
         })
-
         const savedUser = await user.save();
-
         return res.status(200).json({ error: null, data: savedUser, message: "User saved successfully" });
 
     } catch (error) {
         return res.status(500).json({ error: error.message || error, data: null, message: "Error in Registration " })
     }
 }
+
 const login = async (req, res) => {
     const { email, password } = req.body;
-
     try {
-
-
-        const user = await User.findOne({ email: email })
-
+        const user = await User.findOne({ email: email });
         if (!user) {
             return res.status(401).json({ error: "Invalid email or password", data: null, message: "Invalid email or password" });
         }
 
         const matchPassword = await bcrypt.compare(password, user.password);
+
         if (!matchPassword) {
             return res.status(401).json({ error: "Invalid email or password", data: null, message: "Invalid email or password" });
         }
@@ -48,4 +42,5 @@ const login = async (req, res) => {
         return res.status(500).json({ error: error.message || error, data: null, message: "Error in login" })
     }
 }
+
 module.exports = { register, login }
--- src/index.js
+++ src/index.js
@@ -6,20 +6,16 @@ const mongoUri = process.env.MONGO_URI;
 const port = process.env.PORT;
 
 const app = express()
-
-
 app.use(express.json());
 app.use("/api/users", userRoute);
-
 
 mongoose.connect(mongoUri).then(() => {
     app.listen(port, () => {
         console.log(`App is running at port ${port}`);
         console.log("DB is connected");
     })
+}).catch((error) => {
+    console.log("DB connection error", error)
 })
-    .catch((error) => {
-        console.log("DB connection error", error)
-    })
 
 
